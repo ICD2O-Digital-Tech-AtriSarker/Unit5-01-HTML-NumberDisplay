0614ba5cac20be82944bf7ad14ee0c2f6b914797
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-05-29 14:17:03 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Script for Number Display
+
+// ELEMENTS
+// Get Elements from index.html
+const inputForm = document.getElementById("inputForm");
+const minimumInput = document.getElementById("minimumInput");
+const maximumInput = document.getElementById("maximumInput");
+const outputResult = document.getElementById("outputResult");
+
+function getResult() {
+
+  // Clear Output Result
+  outputResult.innerText = "";
+
+  // Get inputs
+  let minimum = Number(minimumInput.value);
+  let maximum = Number(maximumInput.value);
+
+  // Handle edge case of Maximum being smaller than Minimum
+  if (maximum < minimum) {
+    outputResult.innerText = "Maximum value must be greater than minimum value!";
+    return;
+  }
+
+  // Initialize Counter
+  let counter = minimum
+
+  // While Loop
+  while ( counter <= maximum ) {
+    
+    // Display Number
+    outputResult.innerText += counter + "\n";
+    
+    // Increment counter by 1
+    counter += 1;
+    
+  }
+  
+  return;
+}
+
+// Cool function notation
+// form validation + submit call
+inputForm.onsubmit = () => {
+  getResult();
+  return false;
+}
+
